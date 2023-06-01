706c250b5c231f81e816a358eee6206b3b9ccaf6
Pheon-Dev <devpheon@gmail.com>
2023-06-01 19:27:43 +0300

    [+] default error boilerplate and config errors

---
+++ src/error.rs
@@ -0,0 +1,18 @@
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Clone)]
+pub enum Error {
+    // -- Config
+    ConfigMissingEnv(&'static str),
+    ConfigWrongFormat(&'static str),
+}
+
+// region: --- Error Boilerplate
+impl std::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> core::result::Result<(), std::fmt::Error> {
+        write!(fmt, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {}
+// endregion: --- Error Boilerplate
