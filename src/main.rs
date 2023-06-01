10203e3788522ca3eed4ee799b611358c9e60f32
Pheon-Dev <devpheon@gmail.com>
2023-06-01 19:29:03 +0300

    [+] axum router

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use mod config;
+
+pub use self::error::{Error, Result};
+use axum::Router;
+use axum::extract::Query;
+use axum::response::{IntoResponse, Html};
+use axum::routing::get;
+use serde::Deserialize;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let all_routes = Router.merge(routes_hello());
+    Ok(())
 }
