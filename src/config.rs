341a4ad1aabff92f3da08f02edaebcc097518685
Pheon-Dev <devpheon@gmail.com>
2023-06-01 19:27:10 +0300

    [+] configuration for seceret keys and db connection credentials

---
+++ src/config.rs
@@ -0,0 +1,51 @@
+use crate::{Error, Result};
+use once_cell::sync::OnceCell;
+use std::env;
+
+pub fn config() -> &'static Config {
+    static INSTANCE: OnceCell<Config> = OnceCell::new();
+
+    INSTANCE.get_or_init(|| {
+        Config::load_from_env().unwrap_or_else(|ex| {
+
+            panic!("FATAL - WHILE LOADING CONF - Cause: {ex:?}");
+        })
+    })
+}
+
+#[allow(non_snake_case)]
+pub struct Config {
+    // -- Crypt
+    pub PWD_KEY: Vec<u8>,
+
+    pub TOKEN_KEY: Vec<u8>,
+    pub TOKEN_DURATION_SEC: f64,
+
+    // -- DB
+    pub DB_URL: String,
+
+    // -- Web
+    pub WEB_FOLDER: String,
+}
+
+impl Config {
+    fn load_from_env() -> Result<Config> {
+        Ok(Config {
+            // -- Crypt
+            PWD_KEY: get_env_b64u_as_u8s("SERVICE_PWD_KEY")?,
+
+            TOKEN_KEY: get_env_b64u_as_u8s("service_token_key")?,
+            TOKEN_DURATION_SEC: get_env_parse("SERVICE_TOKEN_DURATION_SEC")?,
+
+            // -- DB
+            DB_URL: get_env("SERVICE_DB_URL")?,
+            
+            // -- Web
+            WEB_FOLDER: get_env("SERVICE_WEB_FOLDER")?,
+        })
+    }
+}
+
+fn get_env(name: &'static str) -> Result<String> {
+    env::var(name).map_err(|_| Error::ConfigMissingEnv(name))
+}
