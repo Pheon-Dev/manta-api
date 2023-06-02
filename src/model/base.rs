e8b297b6618df9bde6e5ab64a8b28d71584bc17e
Pheon-Dev <devpheon@gmail.com>
2023-06-03 00:03:51 +0300

    [~] add functionality to the create, get, list, update and delete models

--- src/model/base.rs
+++ src/model/base.rs
@@ -1,7 +1,10 @@
 use crate::ctx::Ctx;
 use crate::model::{Error, ModelManager, Result};
+use crate::utils;
 
 use sqlb::HasFields;
+use sqlx::postgres::PgRow;
+use sqlx::FromRow;
 
 pub trait DbBmc {
     const TABLE: &'static str;
@@ -10,23 +13,115 @@ pub trait DbBmc {
 
 pub async fn create<MC, E>(ctx: &Ctx, mm: &ModelManager, data: E) -> Result<i64>
 where
-MC: DbBmc,
-    E: HasFields, {
-    // let db = mm.db();
-    // let (id, ) = sqlb::insert()
-    //     .table(MC::TABLE)
-    //     .data(fields)
-    //     .returning(&["id"])
-    //     .fetch_one::<_, (i64)>(db)
-    //     .await?;
-    // Ok(id)
-    Ok(89)
+    MC: DbBmc,
+    E: HasFields,
+{
+    let db = mm.db();
+
+    let mut fields = data.not_none_fields();
+
+    if MC::HAS_TIMESTAMPS {
+        let user_id = ctx.user_id();
+        let now = utils::now_utc();
+        fields.push(("cid", user_id).into());
+        fields.push(("ctime", now).into());
+        fields.push(("mid", user_id).into());
+        fields.push(("mtime", now).into());
+    };
+
+    let (id,) = sqlb::insert()
+        .table(MC::TABLE)
+        .data(fields)
+        .returning(&["id"])
+        .fetch_one::<_, (i64,)>(db)
+        .await?;
+
+    Ok(id)
 }
 
-pub async fn get<MC, E>(_ctx: &Ctx, mm: &ModelManager, id: i64) -> Result<()> {Ok(())}
+pub async fn get<MC, E>(_ctx: &Ctx, mm: &ModelManager, id: i64) -> Result<E>
+where
+    MC: DbBmc,
+    E: for<'r> FromRow<'r, PgRow> + Unpin + Send,
+    E: HasFields,
+{
+    let db = mm.db();
+    let entity = sqlb::select()
+        .table(MC::TABLE)
+        .columns(E::field_names())
+        .and_where("id", "=", id)
+        .fetch_optional::<_, E>(db)
+        .await?
+        .ok_or(Error::EntityNotFound {
+            entity: MC::TABLE,
+            id,
+        })?;
 
-pub async fn list<MC, E>() -> Result<()> {Ok(())}
+    Ok(entity)
+}
 
-pub async fn update<MC, E>() -> Result<()> {Ok(())}
+pub async fn list<MC, E>(_ctx: &Ctx, mm: &ModelManager) -> Result<Vec<E>>
+where
+    MC: DbBmc,
+    E: for<'r> FromRow<'r, PgRow> + Unpin + Send,
+    E: HasFields,
+{
+    let db = mm.db();
+    let entities = sqlb::select()
+        .table(MC::TABLE)
+        .columns(E::field_names())
+        .order_by("id")
+        .fetch_all::<_, E>(db)
+        .await?;
 
-pub async fn delete<MC, E>() -> Result<()> {Ok(())}
+    Ok(entities)
+}
+
+pub async fn update<MC, E>(ctx: &Ctx, mm: &ModelManager, id: i64, data: E) -> Result<()>
+where
+    MC: DbBmc,
+    E: HasFields,
+{
+    let db = mm.db();
+
+    let mut fields = data.not_none_fields();
+
+    if MC::HAS_TIMESTAMPS {
+        let user_id = ctx.user_id();
+        let now = utils::now_utc();
+        fields.push(("mid", user_id).into());
+        fields.push(("mtime", now).into());
+    }
+
+    let count = sqlb::update()
+        .table(MC::TABLE)
+        .data(fields)
+        .and_where("id", "=", id)
+        .exec(db)
+        .await?;
+
+    if count == 0 {
+        Err(Error::EntityNotFound {
+            entity: MC::TABLE,
+            id,
+        })
+    } else {
+        Ok(())
+    }
+}
+
+pub async fn delete<MC, E>(_ctx: &Ctx, mm: &ModelManager, id: i64) -> Result<()> where MC: DbBmc {
+    let db = mm.db();
+
+    let count = sqlb::delete()
+        .table(MC::TABLE)
+        .and_where("id", "=", id)
+        .exec(db)
+        .await?;
+
+    if count == 0 {
+        Err(Error::EntityNotFound { entity: MC::TABLE, id })
+    } else {
+        Ok(())
+    }
+}
