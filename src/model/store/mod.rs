7dd965f4bec7e8482a29d331951d5c3ffc1ee8ab
Pheon-Dev <devpheon@gmail.com>
2023-06-02 13:48:21 +0300

    [+] model store and error module

---
+++ src/model/store/mod.rs
@@ -0,0 +1,17 @@
+mod error;
+
+pub use self::error::{Error, Result};
+
+use crate::config;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
+
+pub type Db = Pool<Postgres>;
+
+pub async fn new_db_pool() -> Result<Db> {
+    PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&config().DB_URL)
+        .await
+        .map_err(|ex| Error::FailToCreatePool(ex.to_string()))
+}
