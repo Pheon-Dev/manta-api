56ac9ac8bf2bfc8b739da1e1c5f5e0668db224f6
Pheon-Dev <devpheon@gmail.com>
2023-06-02 22:19:05 +0300

    [+] ModelManager and Router connection

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,45 @@
+// region: --- Modules
 mod config;
+mod web;
 mod error;
+mod model;
+mod ctx;
+
+// pub mod _dev_utils;
 
 pub use self::error::{Error, Result};
+pub use config::config;
+
+use crate::model::ModelManager;
+use axum::{middleware, Router};
+use tracing_subscriber::EnvFilter;
+use std::net::SocketAddr;
+
+// endregion: --- Modules
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .without_time()
+        .with_target(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    // -- FOR DEV ONLY 
+    // _dev_utils::init_dev().await;
+
+    let mm = ModelManager::new().await?;
+
+    let all_routes = Router::new()
+        .merge(web::login_routes::routes(mm.clone()))
+        .fallback_service(web::static_routes::serve_dir());
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    info!("{:<12} - on {addr}\n", "LISTENING");
+    axum::Server::bind(&addr)
+        .serve(all_routes.into_make_service())
+        .await
+        .unwrap();
+
     Ok(())
 }
