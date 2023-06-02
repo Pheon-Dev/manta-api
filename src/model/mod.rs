7e13d64d118f941f76420d81ca9e68ca91483c88
Pheon-Dev <devpheon@gmail.com>
2023-06-02 22:16:39 +0300

    [+] ModelManager layer

---
+++ src/model/mod.rs
@@ -0,0 +1,42 @@
+//! Model Layer
+//!
+//! Design:
+//!
+//! - The Model layer normalizes the application's data type structures and access.
+//! - All application code data access must go through the Model layer.
+//! - The `ModelManager` holds the internal states/resources needed by ModelControllers
+//!   to access data. (e.g., db_pool, S3 client, redis client).
+//! - Model Controllers (e.g., `TaskBmc`, `ProjectBmc`) implement
+//!   CRUD and other data access methods on a given "entity" (e.g., `Task`, `Project`).
+//!   (`Bmc` is short for Backend Model Controller).
+//! - In frameworks like Axum, Tauri, `ModelManager` are typically used as App State.
+//! - ModelManager are designed to be passed as an argument to all Model Controllers functions.
+//!
+
+mod base;
+mod error;
+mod store;
+
+pub mod task;
+pub mod user;
+
+pub use self::error::{Error, Result};
+
+use crate::model::store::{new_db_pool, Db};
+
+#[derive(Clone)]
+pub struct ModelManager {
+    db: Db,
+}
+
+impl ModelManager {
+    pub async fn new() -> Result<Self> {
+        let db = new_db_pool().await?;
+
+        Ok(Self { db })
+    }
+
+    pub(in crate::model) fn db(&self) -> &Db {
+        &self.db
+    }
+}
