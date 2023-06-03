801aa4b4ef9ec40aec01a555cc2b1366f69a5c0e
Pheon-Dev <devpheon@gmail.com>
2023-06-03 11:33:45 +0300

    [+] crypt error module

---
+++ src/crypt/error.rs
@@ -0,0 +1,28 @@
+use serde::Serialize;
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug, Serialize)]
+pub enum Error {
+    // -- Key
+    KeyFailHmac,
+
+    // -- Pwd
+    PwdNotMatching,
+    SchemeUnknown(String),
+    SchemeNotFoundInContent,
+    // -- Token
+}
+
+// region: --- Error Boilerplate
+impl std::fmt::Display for Error {
+    fn fmt(
+        &self,
+        fmt: &mut std::fmt::Formatter,
+    ) -> core::result::Result<(), std::fmt::Error> {
+        write!(fmt, "self:?")
+    }
+}
+
+impl std::error::Error for Error {}
+// endregion: --- Error Boilerplate
