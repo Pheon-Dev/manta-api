ceb3b51cde67fb69a3695b0f362f227f7874a410
Pheon-Dev <devpheon@gmail.com>
2023-06-03 11:33:32 +0300

    [+] crypt crate

---
+++ src/crypt/mod.rs
@@ -0,0 +1,32 @@
+mod error;
+pub mod pwd;
+
+pub use self::error::{Error, Result};
+
+use hmac::{Hmac, Mac};
+use sha2::Sha512;
+
+pub struct EncryptContent {
+    pub content: String,
+    pub salt: String,
+}
+
+pub fn encrypt_into_b64u(
+    key: &[u8],
+    enc_content: &EncryptContent,
+) -> Result<String> {
+    let EncryptContent {content, salt} = enc_content;
+
+    // -- create a HMAC-SHA-512 from key.
+    let mut hmac_sha512 = Hmac::<Sha512>::new_from_slice(key).map_err(|_| Error::KeyFailHmac)?;
+
+    // -- Add content 
+    hmac_sha512.update(content.as_bytes());
+    hmac_sha512.update(salt.as_bytes());
+
+    // -- Finalize and b64u encode
+    let hmac_result = hmac_sha512.finalize();
+    let result_bytes = hmac_result.into_bytes();
+    let result = base64_url::encode(&result_bytes);
+    Ok(result)
+}
