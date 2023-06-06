38450c1f8a80211fa2bfd6fb3813a4d051c82a7a
Pheon-Dev <devpheon@gmail.com>
2023-06-06 15:09:45 +0300

    [+] renaming send endpoints to send_req

---
+++ src/web/rpc/send_req_rpc.rs
@@ -0,0 +1,21 @@
+use crate::Result;
+
+pub async fn create_send_req() -> Result<()> {
+    todo!();
+    Ok(())
+}
+
+pub async fn list_send_reqs() -> Result<()> {
+    todo!();
+    Ok(())
+}
+
+pub async fn update_send_req() -> Result<()> {
+    todo!();
+    Ok(())
+}
+
+pub async fn delete_send_req() -> Result<()> {
+    todo!();
+    Ok(())
+}
