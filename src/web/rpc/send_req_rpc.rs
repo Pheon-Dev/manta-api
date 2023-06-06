09d155424cd46717832a4a1294a8303507b1bbb2
Pheon-Dev <devpheon@gmail.com>
2023-06-06 16:19:45 +0300

    [~] create, list, update and delete Send Request RPCs

--- src/web/rpc/send_req_rpc.rs
+++ src/web/rpc/send_req_rpc.rs
@@ -1,21 +1,37 @@
-use crate::Result;
+use crate::ctx::Ctx;
+use crate::model::send_req::{SendReq, SendReqForCreate, SendReqBmc, SendReqForUpdate};
+use crate::model::ModelManager;
+use crate::web::rpc::{ParamsForCreate, ParamsForUpdate, ParamsIded};
+use crate::web::Result;
 
-pub async fn create_send_req() -> Result<()> {
-    todo!();
-    Ok(())
+pub async fn create_send_req(ctx: Ctx, mm: ModelManager, params: ParamsForCreate<SendReqForCreate>) -> Result<SendReq> {
+    let ParamsForCreate { data } = params;
+
+    let id = SendReqBmc::create(&ctx, &mm, data).await?;
+    let send_req = SendReqBmc::get(&ctx, &mm, id).await?;
+    Ok(send_req)
 }
 
-pub async fn list_send_reqs() -> Result<()> {
-    todo!();
-    Ok(())
+pub async fn list_send_reqs(mm: ModelManager, ctx: Ctx) -> Result<Vec<SendReq>> {
+    let send_reqs = SendReqBmc::list(&ctx, &mm).await?;
+    Ok(send_reqs)
 }
 
-pub async fn update_send_req() -> Result<()> {
-    todo!();
-    Ok(())
+pub async fn update_send_req(ctx: Ctx, mm: ModelManager, params: ParamsForUpdate<SendReqForUpdate>,) -> Result<SendReq> {
+    let ParamsForUpdate { id, data } = params;
+
+    SendReqBmc::update(&ctx, &mm, id, data).await?;
+
+    let send_req = SendReqBmc::get(&ctx, &mm, id).await?;
+
+    Ok(send_req)
 }
 
-pub async fn delete_send_req() -> Result<()> {
-    todo!();
-    Ok(())
+pub async fn delete_send_req(ctx: Ctx, mm: ModelManager, params: ParamsIded) -> Result<SendReq> {
+    let ParamsIded { id } = params;
+
+    let send_req = SendReqBmc::get(&ctx, &mm, id).await?;
+    SendReqBmc::delete(&ctx, &mm, id).await?;
+
+    Ok(send_req)
 }
