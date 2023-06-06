34fb23a09a2765df8ab9bd68025d040a594ba993
Pheon-Dev <devpheon@gmail.com>
2023-06-06 12:08:49 +0300

    [~] formatting code

--- src/web/login_routes.rs
+++ src/web/login_routes.rs
@@ -4,7 +4,7 @@ use crate::ctx::Ctx;
 use crate::model::user::{UserBmc, UserForLogin};
 use crate::model::ModelManager;
 use crate::web;
-use crate::web::{Result, Error};
+use crate::web::{Error, Result};
 
 use axum::extract::State;
 use axum::routing::post;
@@ -16,7 +16,7 @@ use tracing::debug;
 
 pub fn routes(mm: ModelManager) -> Router {
     Router::new()
-    .route("/api/login", post(login_api_handler))
+        .route("/api/login", post(login_api_handler))
         .with_state(mm)
 }
 
@@ -28,9 +28,55 @@ async fn login_api_handler(
 ) -> Result<Json<Value>> {
     debug!("{:<12} - api_login_handler", "HANLDER");
 
-    let LoginPayload { username, pwd: pwd_clear } = payload;
+    let LoginPayload {
+        username,
+        pwd: pwd_clear,
+    } = payload;
     let root_ctx = Ctx::root_ctx();
 
     // -- Get the user
-    let user: UserForLogin
+    let user: UserForLogin = UserBmc::first_by_username(&root_ctx, &mm, &username)
+        .await?
+        .ok_or(Error::LoginFailUsernameNotFound)?;
+
+    let user_id = user.id;
+
+    // -- Validate the password
+    let Some(pwd) = user.pwd else {
+        return Err(Error::LoginFailUserHasNoPwd{user_id});
+    };
+
+    let scheme_status = pwd::validate_pwd(
+        &EncryptContent {
+            salt: user.pwd_salt.to_string(),
+            content: pwd_clear.clone(),
+        },
+        &pwd,
+    )
+    .map_err(|_| Error::LoginFailPwdNotMatching { user_id })?;
+
+    // -- If pwd scheme is outdated, update pwd
+    if let SchemeStatus::Outdated = scheme_status {
+        debug!("pwd encrypt scheme outdated, upgrading ...");
+        UserBmc::update_pwd(&root_ctx, &mm, user.id, &pwd_clear).await?;
+    }
+
+    // -- Generate the web token
+    web::set_token_cookie(&cookies, &user.username, &user.token_salt.to_string())?;
+
+    // -- Create the success body
+    let body = Json(json!({
+        "result": {
+        "success": true
+    }
+    }));
+
+    Ok(body)
 }
+
+#[derive(Debug, Deserialize)]
+struct LoginPayload {
+    username: String,
+    pwd: String,
+}
+// endregion: --- Handler
