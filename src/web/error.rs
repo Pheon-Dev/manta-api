798b49be6f689524b85338765bf10a122415b6b8
Pheon-Dev <devpheon@gmail.com>
2023-06-06 10:37:44 +0300

    [+] context authentication error

--- src/web/error.rs
+++ src/web/error.rs
@@ -12,11 +12,13 @@ pub enum Error {
     // TODO: RPC
     // login
     LoginFailUsernameNotFound,
-    LoginFailUserHasNoPwd {user_id: i64},
-    LoginFailPwdNotMatching {user_id: i64},
+    LoginFailUserHasNoPwd { user_id: i64 },
+    LoginFailPwdNotMatching { user_id: i64 },
 
     // TODO: middleware/extractor
     // TODO: ctxAuthError
+    CtxAuth(web::mw_auth::CtxAuthError),
+
     // TODO: modules
     Model(model::Error),
     Crypt(crypt::Error),
@@ -31,10 +33,7 @@ impl From<model::Error> for Error {
 }
 
 impl core::fmt::Display for Error {
-    fn fmt (
-        &self,
-        fmt: &mut core::fmt::Formatter,
-    ) -> core::result::Result<(), core::fmt::Error> {
+    fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
         write!(fmt, "{self:?}")
     }
 }
@@ -46,20 +45,33 @@ impl Error {
         use web::Error::*;
 
         match self {
-
+            // -- Login
             LoginFailUsernameNotFound
             | LoginFailUserHasNoPwd { .. }
-            | LoginFailPwdNotMatching { .. } => {
-                    (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL)
-                }
+            | LoginFailPwdNotMatching { .. } => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
 
-            // Fallback
-            _ => (StatusCode::INTERNAL_SERVER_ERROR, ClientError::SERVICE_ERROR),
+            // --  Auth
+            CtxAuth(_) => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+
+            // -- Model
+            Model(model::Error::EntityNotFound { entity, id }) => (
+                StatusCode::BAD_REQUEST,
+                ClientError::EntityNotFound { entity, id: *id },
+            ),
+            Model(model::Error::UserAlreadyExists { .. }) => {
+                (StatusCode::BAD_REQUEST, ClientError::USER_ALREADY_EXISTS)
+            }
+
+            // -- Fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
         }
     }
 }
 
-/// This is the ClientError use to be serialised in the 
+/// This is the ClientError use to be serialised in the
 /// json-rpc error body.
 /// Only used in the mw_res_mapper
 #[derive(Debug, Serialize, strum_macros::AsRefStr)]
@@ -69,6 +81,6 @@ pub enum ClientError {
     USER_ALREADY_EXISTS,
     LOGIN_FAIL,
     NO_AUTH,
-    EntityNotFound { entity: &'static str, id: i64},
+    EntityNotFound { entity: &'static str, id: i64 },
     SERVICE_ERROR,
 }
