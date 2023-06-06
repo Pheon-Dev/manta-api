a1b729f6738e5c58ec29fa0085030fcd75457e57
Pheon-Dev <devpheon@gmail.com>
2023-06-06 14:09:03 +0300

    [+] RPC Info

--- src/web/mw_res_map.rs
+++ src/web/mw_res_map.rs
@@ -1,8 +1,12 @@
 use crate::ctx::Ctx;
+use crate::web::rpc::RpcInfo;
+use crate::log::log_request;
 use crate::web::{self, ReqStamp};
 
 use axum::http::{Method, Uri};
 use axum::response::{IntoResponse, Response};
+use axum::Json;
+use serde_json::{json, to_value};
 use tracing::{debug, error};
 
 pub async fn mw_response_map(
@@ -21,7 +25,50 @@ pub async fn mw_response_map(
     let client_status_error = web_error.map(|se| se.client_status_and_error());
 
     // -- If client error, build the new response
-    // let error_response = 
-    // error_response.unwrap_or(res)
-    todo!();
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error = to_value(client_error).ok();
+            let message = client_error.as_ref().and_then(|v| v.get("message"));
+            let detail = client_error.as_ref().and_then(|v| v.get("detail"));
+
+            let client_error_body = json!({
+                "id": rpc_info.as_ref().map(|rpc| rpc.id.clone()),
+                "error": {
+                // TODO: Will need to follow json-rpc error code practices.
+                    "code": 0,
+                // In our design error.message == enum variant name
+                    "message": message,
+                    "data": {
+                        "req_uuid": req_stamp.uuid.to_string(),
+                        "detail": detail
+                    }
+                }
+            });
+
+            debug!("CLIENT ERROR BODY: \n{client_error_body}");
+
+            // Build the new response from the client_error_body
+            (*status_code, Json(client_error_body)).into_response()
+        });
+    // -- build and log the server log line
+    let client_error = client_status_error.unzip().1;
+    if let Err(log_err) = log_request(
+        http_method,
+        uri,
+        req_stamp,
+        rpc_info,
+        ctx,
+        web_error,
+        client_error,
+    )
+        .await {
+        error!("CRITICAL ERROR - COULD NOT LOG_REQUEST - {log_err:?}");
+    };
+
+    // The empty line
+    debug!("\n");
+
+    // -- Return the appropriate response
+    error_response.unwrap_or(res)
 }
