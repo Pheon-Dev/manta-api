987a256d2a9bd0a701438c618c4024394fd0d5e1
Pheon-Dev <devpheon@gmail.com>
2023-07-01 23:28:56 +0300

    [+] dev utils for testing only

--- src/_dev_utils/dev_db.rs
+++ src/_dev_utils/dev_db.rs
@@ -1,115 +1,110 @@
-use crate::crypt::pwd::{self};
-use crate::crypt::EncryptContent;
-
+use crate::ctx::Ctx;
+use crate::model::user::{User, UserBmc};
+use crate::model::ModelManager;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
 use std::fs;
 use std::path::PathBuf;
 use std::time::Duration;
 use tracing::info;
-use uuid::Uuid;
 
 type Db = Pool<Postgres>;
 
-// NOTE: Hardcore to prevent deployed system db update.
-const PG_DEV_POSTGRES_URL: &str = "postgres://postgres:welcome@localhost:5432/postgres";
+// NOTE: Harcode to prevent deployed system db update.
+const PG_DEV_POSTGRES_URL: &str = "postgres://postgres:welcome@localhost/postgres";
 const PG_DEV_APP_URL: &str = "postgres://app_user:dev_only_pwd@localhost/app_db";
 
 // sql files
 const SQL_RECREATE_DB: &str = "sql/dev_initial/00-recreate-db.sql";
 const SQL_DIR: &str = "sql/dev_initial";
 
+const DEMO_PWD: &str = "welcome";
+
 pub async fn init_dev_db() -> Result<(), Box<dyn std::error::Error>> {
-    info!("{:<12} - init_dev_db()", "FOR-DEV-ONLY");
+	info!("{:<12} - init_dev_db()", "FOR-DEV-ONLY");
 
-    // -- Create the app_db/app_user with postgres user.
-    {
-        let root_db = new_db_pool(PG_DEV_POSTGRES_URL).await?;
-        pexec(&root_db, SQL_RECREATE_DB).await?;
-    }
+	// -- Create the app_db/app_user with posgres user.
+	{
+		let root_db = new_db_pool(PG_DEV_POSTGRES_URL).await?;
+		pexec(&root_db, SQL_RECREATE_DB).await?;
+	}
 
-    // -- Get sql files.
-    let mut paths: Vec<PathBuf> = fs::read_dir(SQL_DIR)?
-        .filter_map(|e| e.ok().map(|e| e.path()))
-        .collect();
-    paths.sort();
+	// -- Get sql files.
+	let mut paths: Vec<PathBuf> = fs::read_dir(SQL_DIR)?
+		.filter_map(|e| e.ok().map(|e| e.path()))
+		.collect();
+	paths.sort();
 
-    // -- SQL Execute eaxh file
-    let app_db = new_db_pool(PG_DEV_APP_URL).await?;
-    for path in paths {
-        if let Some(path) = path.to_str() {
-            let path = path.replace('\\', "/");
+	// -- SQL Execute each file.
+	let app_db = new_db_pool(PG_DEV_APP_URL).await?;
+	for path in paths {
+		if let Some(path) = path.to_str() {
+			let path = path.replace('\\', "/"); // for Windows.
 
-            // Only take .sql and skip the SQL_RECREATE_DB
-            if path.ends_with(".sql") && path != SQL_RECREATE_DB {
-                pexec(&app_db, &path).await?;
-            }
-        }
-    }
+			// Only take .sql and skip the SQL_RECREATE
+			if path.ends_with(".sql") && path != SQL_RECREATE_DB {
+				pexec(&app_db, &path).await?;
+			}
+		}
+	}
 
-    // -- Set the "welcome" password to demo1
-    let (id, salt): (i64, Uuid) = sqlx::query_as(
-        r#"
-    SELECT id, pwd_salt FROM "user"
-    WHERE username = $1
-    "#,
-    )
-    .bind("demo1")
-    .fetch_one(&app_db)
-    .await?;
+	// -- Init model layer.
+	let mm = ModelManager::new().await?;
+	let ctx = Ctx::root_ctx();
 
-    let pwd = pwd::encrypt_pwd(&EncryptContent {
-        salt: salt.to_string(),
-        content: "welcome".to_string(),
-    })?;
-    sqlx::query(r#"UPDATE "user" SET pwd = $1 WHERE id = $2"#)
-        .bind(pwd)
-        .bind(id)
-        .execute(&app_db)
-        .await?;
+	// -- Set demo1 pwd
+	let demo1_user: User = UserBmc::first_by_username(&ctx, &mm, "demo1")
+		.await?
+		.unwrap();
+	UserBmc::update_pwd(&ctx, &mm, demo1_user.id, DEMO_PWD).await?;
+	info!("{:<12} - init_dev_db - set demo1 pwd", "FOR-DEV-ONLY");
 
-    Ok(())
+	Ok(())
 }
 
 async fn pexec(db: &Db, file: &str) -> Result<(), sqlx::Error> {
-    info!("{:<12} - pexec: {file}", "FOR-DEV-ONLY");
+	info!("{:<12} - pexec: {file}", "FOR-DEV-ONLY");
 
-    // Read the file
-    let content = fs::read_to_string(file)?;
+	// -- Read the file.
+	let content = fs::read_to_string(file)?;
 
-    // TODO: Make the split more sql proof
-    let sqls: Vec<&str> = content.split(';').collect();
+	// TODO: Make the split more sql proof.
+	let sqls: Vec<&str> = content.split(';').collect();
 
-    let mut fn_sql_parts: Vec<&str> = Vec::new();
-    for sql in sqls {
-        // -- Trick to not split function body
-        // TODO: (Needs to be robust)
-        // FIXME: This works for simple sql files with trigger $$ notation.
-        // However, it will probably break for other specific cases.
-        // It needs to be made more robust.
-        // sqlx does not seem to have a non static file executor
-        // If it is at the beginning of a function, we need to start keeping track
-        if sql.contains("BEGINS") {
-            fn_sql_parts.push(sql);
-        } else if !fn_sql_parts.is_empty() {
-            fn_sql_parts.push(sql);
+	// -- SQL Execute each part.
+	let mut fn_sql_parts: Vec<&str> = Vec::new();
+	for sql in sqls {
+		// -- Trick to not split function body
+		//    (TODO: Needs to be make it more robust.)
 
-            if sql.trim().starts_with("$$") {
-                let sql = format!("{};", fn_sql_parts.join(";"));
-                sqlx::query(&sql).execute(db).await?;
-                fn_sql_parts.clear();
-            }
-        } else {
-            sqlx::query(sql).execute(db).await?;
-        }
-    }
-    Ok(())
+		// FIXME: This works for simple sql files with trigger with $$ notation.
+		//        However, will probably break for other specific cases.
+		//        It needs to be made more robust.
+		//        sqlx does not seems to have a non static file executor.
+		// If it is the begin of a function we start keeping track
+		if sql.contains("BEGIN") {
+			fn_sql_parts.push(sql);
+		} else if !fn_sql_parts.is_empty() {
+			fn_sql_parts.push(sql);
+
+			// Here we assume the end will be `$$ LANGUAGE plpgsql;`
+			if sql.trim().starts_with("$$") {
+				let sql = format!("{};", fn_sql_parts.join(";"));
+				sqlx::query(&sql).execute(db).await?;
+				fn_sql_parts.clear();
+			}
+		} else {
+			sqlx::query(sql).execute(db).await?;
+		}
+	}
+
+	Ok(())
 }
 
 async fn new_db_pool(db_con_url: &str) -> Result<Db, sqlx::Error> {
-    PgPoolOptions::new()
-        .max_connections(1)
-        .acquire_timeout(Duration::from_millis(500))
-        .connect(db_con_url)
-        .await
+	PgPoolOptions::new()
+		.max_connections(1)
+		.acquire_timeout(Duration::from_millis(500))
+		.connect(db_con_url)
+		.await
 }
--- src/_dev_utils/mod.rs
+++ src/_dev_utils/mod.rs
@@ -1,70 +1,57 @@
+// region:    --- Modules
+
 mod dev_db;
 
 use crate::ctx::Ctx;
-use crate::model::send_req::{SendReq, SendReqBmc, SendReqForCreate};
+use crate::model::send::{Send, SendBmc, SendForCreate};
 use crate::model::{self, ModelManager};
-
 use tokio::sync::OnceCell;
 use tracing::info;
 
-// Initialize environment for local development
+// endregion: --- Modules
+
+// Initialize environment for local development.
 // (for early development, called from `main()`)
 pub async fn init_dev() {
-    static INIT: OnceCell<()> = OnceCell::const_new();
+	static INIT: OnceCell<()> = OnceCell::const_new();
 
-    INIT.get_or_init(|| async {
-        info!("{:<12} - init_dev()", "FOR-DEV-ONLY");
+	INIT.get_or_init(|| async {
+		info!("{:<12} - init_dev()", "FOR-DEV-ONLY");
 
-        dev_db::init_dev_db().await.unwrap();
-    })
-    .await;
+		dev_db::init_dev_db().await.unwrap();
+	})
+	.await;
 }
 
-/// Initialize test environment
+/// Initialize test environment.
 pub async fn init_test() -> ModelManager {
-    static INIT: OnceCell<ModelManager> = OnceCell::const_new();
+	static INIT: OnceCell<ModelManager> = OnceCell::const_new();
 
-    let mm = INIT
-        .get_or_init(|| async {
-            init_dev().await;
-            ModelManager::new().await.unwrap()
-        })
-        .await;
+	let mm = INIT
+		.get_or_init(|| async {
+			init_dev().await;
+			ModelManager::new().await.unwrap()
+		})
+		.await;
 
-    mm.clone()
+	mm.clone()
 }
 
-pub async fn seed_tasks(
-    ctx: &Ctx,
-    mm: &ModelManager,
-    amounts: &[&str],
-    currencies: &[&str],
-    receivers: &[&str],
-    descriptions: &[&str],
-) -> model::Result<Vec<SendReq>> {
-    let mut send_reqs = Vec::new();
+pub async fn seed_sends(
+	ctx: &Ctx,
+	mm: &ModelManager,
+	amounts: &[&str],
+) -> model::Result<Vec<Send>> {
+	let mut sends = Vec::new();
 
-    for amount in amounts {
-        for currency in currencies {
-            for receiver in receivers {
-                for description in descriptions {
-                    let id = SendReqBmc::create(
-                        ctx,
-                        mm,
-                        SendReqForCreate {
-                            amount: amount.to_string(),
-                            currency: currency.to_string(),
-                            receiver: receiver.to_string(),
-                            description: description.to_string(),
-                        },
-                    )
-                    .await?;
-                    let send_req = SendReqBmc::get(ctx, mm, id).await?;
-                    send_reqs.push(send_req);
-                }
-            }
-        }
-    }
+	for amount in amounts {
+		let id =
+			SendBmc::create(ctx, mm, SendForCreate { amount: amount.to_string() })
+				.await?;
+		let send = SendBmc::get(ctx, mm, id).await?;
 
-    Ok(send_reqs)
+		sends.push(send);
+	}
+
+	Ok(sends)
 }
