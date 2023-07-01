cfcfdb6ab9d69e2c4d1ca9924cbb7447fb8a4def
Pheon-Dev <devpheon@gmail.com>
2023-07-01 23:30:33 +0300

    [+] utilities eg time

--- src/utils/error.rs
+++ src/utils/error.rs
@@ -4,17 +4,22 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Debug, Serialize)]
 pub enum Error {
-    FailToB64uDecode,
-    DateFailParse(String),
+	// -- Time
+	DateFailParse(String),
+
+	// -- Base64
+	FailToB64uDecode,
 }
 
+// region:    --- Error Boilerplate
 impl core::fmt::Display for Error {
-    fn fmt(
-        &self,
-        fmt: &mut core::fmt::Formatter,
-    ) -> core::result::Result<(), core::fmt::Error> {
-        write!{fmt, "{self:?}"}
-    }
+	fn fmt(
+		&self,
+		fmt: &mut core::fmt::Formatter,
+	) -> core::result::Result<(), core::fmt::Error> {
+		write!(fmt, "{self:?}")
+	}
 }
 
 impl std::error::Error for Error {}
+// endregion: --- Error Boilerplate
