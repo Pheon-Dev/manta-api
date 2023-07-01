7a4344f6e41f5996c21c1a85ac35b69c40c60ade
Pheon-Dev <devpheon@gmail.com>
2023-07-01 23:28:40 +0300

    [+] sql file migrations

--- sql/02-dec-seed.sql
+++
@@ -1,4 +0,0 @@
--- User demo1
-INSERT INTO "user" 
-  (username, cid, ctime, mid, mtime) VALUES
-  ('demo1', 0, now(), 0, now());
--- sql/dev_initial/00-recreate-db.sql
+++ sql/dev_initial/00-recreate-db.sql
@@ -1,9 +1,9 @@
 -- DEV ONLY - Brute Force DROP DB (for local dev and unit test)
 SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE
-  username = 'app_user' OR dataname = 'app_db';
+ usename = 'app_user' OR datname = 'app_db';
 DROP DATABASE IF EXISTS app_db;
 DROP USER IF EXISTS app_user;
 
--- DEV ONLY - Dev only password (for local dev and unit test)
+-- DEV ONLY - Dev only password (for local dev and unit test).
 CREATE USER app_user PASSWORD 'dev_only_pwd';
-CREATE DATABASE app_db OWNER app_user ENCODING = 'UTF8';
+CREATE DATABASE app_db owner app_user ENCODING = 'UTF-8';
