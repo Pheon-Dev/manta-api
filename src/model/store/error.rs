d1fec69bfbf6381a023e9e4b2f5a75e2be63703b
Pheon-Dev <devpheon@gmail.com>
2023-07-01 23:30:05 +0300

    [+] store connected to the db

--- src/model/store/error.rs
+++ src/model/store/error.rs
@@ -4,13 +4,18 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Debug, Serialize)]
 pub enum Error {
-    FailToCreatePool(String),
+	FailToCreatePool(String),
 }
 
+// region:    --- Error Boilerplate
 impl core::fmt::Display for Error {
-    fn fmt(&self, f: &mut core::fmt::Formatter,) -> core::result::Result<(), core::fmt::Error> {
-        write!(f, "{self:?}")
-}
+	fn fmt(
+		&self,
+		fmt: &mut core::fmt::Formatter,
+	) -> core::result::Result<(), core::fmt::Error> {
+		write!(fmt, "{self:?}")
+	}
 }
 
 impl std::error::Error for Error {}
+// endregion: --- Error Boilerplate
--- src/model/store/mod.rs
+++ src/model/store/mod.rs
@@ -1,3 +1,5 @@
+// region:    --- Modules
+
 mod error;
 
 pub use self::error::{Error, Result};
@@ -6,12 +8,18 @@ use crate::config;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
 
+// endregion: --- Modules
+
+// region:    --- Db Store
+
 pub type Db = Pool<Postgres>;
 
 pub async fn new_db_pool() -> Result<Db> {
-    PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&config().DB_URL)
-        .await
-        .map_err(|ex| Error::FailToCreatePool(ex.to_string()))
+	PgPoolOptions::new()
+		.max_connections(5)
+		.connect(&config().DB_URL)
+		.await
+		.map_err(|ex| Error::FailToCreatePool(ex.to_string()))
 }
+
+// endregion: --- Db Store
