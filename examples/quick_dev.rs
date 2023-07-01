60e5ab8f94ce112f7f7c6e34beb4ca0943721fe3
Pheon-Dev <devpheon@gmail.com>
2023-07-01 23:28:23 +0300

    [+] test run on a mock up

--- examples/quick_dev.rs
+++ examples/quick_dev.rs
@@ -1,80 +1,80 @@
-#![allow(unused)]
+#![allow(unused)] // For beginning only.
 
 use anyhow::Result;
 use serde_json::json;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let hc = httpc_test::new_client("http://localhost:8080")?;
+	let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    // hc.do_get("/index.html").await?.print().await?;
+	// hc.do_get("/index.html").await?.print().await?;
 
-    let req_login = hc.do_post(
-        "/api/login",
-        json!({
-            "username": "demo1",
-            "pwd": "welcome"
-        }),
-    );
+	let req_login = hc.do_post(
+		"/api/login",
+		json!({
+			"username": "demo1",
+			"pwd": "welcome"
+		}),
+	);
+	req_login.await?.print().await?;
 
-    req_login.await?.print().await?;
+	let req_create_send = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "create_send",
+			"params": {
+				"data": {
+					"amount": "100"
+				}
+			}
+		}),
+	);
+	req_create_send.await?.print().await?;
 
-    let req_create_send_req = hc.do_post(
-        "/api/rpc",
-        json!({
-            "id": null,
-            "method": "create_send_req",
-            "params": {
-                "data": {
-                    "amount": "100",
-                    "currency": "KES",
-                    "receiver": "John",
-                    "description": "Lunch",
-                }
-            }
-        }),
-    );
+	let req_update_send = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "update_send",
+			"params": {
+				"id": 1000, // Hardcode the send id.
+				"data": {
+					"amount": "200"
+				}
+			}
+		}),
+	);
+	req_update_send.await?.print().await?;
 
-    req_create_send_req.await?.print().await?;
+	let req_delete_send = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "delete_send",
+			"params": {
+				"id": 1001 // Harcode the send id
+			}
+		}),
+	);
+	req_delete_send.await?.print().await?;
 
-    let req_update_send_req = hc.do_post(
-        "/api/rpc",
-        json!({
-            "id": 1,
-            "method": "update_send_req",
-            "params": {
-                "id": 1,
-                "data": {
-                    "currency": "USD",
-                }
-            }
-        }),
-    );
+	let req_list_sends = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "list_sends"
+		}),
+	);
+	req_list_sends.await?.print().await?;
 
-    req_update_send_req.await?.print().await?;
+	// let req_logoff = hc.do_post(
+	// 	"/api/logoff",
+	// 	json!({
+	// 		"logoff": true
+	// 	}),
+	// );
+	// req_logoff.await?.print().await?;
 
-    let req_delete_send_req = hc.do_post(
-        "/api/rpc",
-        json!({
-            "id": null,
-            "method": "delete_send_req",
-            "params": {
-                "id": 1,
-            }
-        }),
-    );
-
-    // req_delete_send_req.await?.print().await?;
-
-    let req_list_send_req = hc.do_post(
-        "/api/rpc",
-        json!({
-            "id": 2,
-            "method": "list_send_req",
-        }),
-    );
-
-    req_list_send_req.await?.print().await?;
-
-    Ok(())
+	Ok(())
 }
