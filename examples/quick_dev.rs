25b0a2991bb9c631752f124cc883ee2a6892f348
Pheon-Dev <devpheon@gmail.com>
2023-07-17 08:52:06 +0300

    [/] pwd → password [-] completed

--- examples/quick_dev.rs
+++ examples/quick_dev.rs
@@ -7,15 +7,13 @@ use serde_json::json;
 async fn main() -> Result<()> {
 	let hc = httpc_test::new_client("http://localhost:8080")?;
 
-	hc.do_get("/hello2/Mike").await?.print().await?;
-
-	hc.do_get("/src/main.rs").await?.print().await?;
+	// hc.do_get("/src/main.rs").await?.print().await?;
 
 	let req_login = hc.do_post(
 		"/api/login",
 		json!({
 			"username": "demo1",
-			"pwd": "welcome"
+			"password": "welcome"
 		}),
 	);
 	req_login.await?.print().await?;
@@ -23,11 +21,10 @@ async fn main() -> Result<()> {
 	let req_create_payment = hc.do_post(
 		"/api/payments",
 		json!({
-			"amount": "1000",
+			"amount": "5000",
 			"sender": "john",
 			"receiver": "jane",
-			"description": "lunch",
-			"completed": true,
+			"description": "shopping",
 		}),
 	);
 	req_create_payment.await?.print().await?;
