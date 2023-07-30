45a5b8b53229545129cb71ac9e4aba695366d7b9
Pheon-Dev <devpheon@gmail.com>
2023-07-30 19:37:53 +0300

    [+] handle send money callback

--- web-folder/wallet-web-app/src/pages/wallet/Send.tsx
+++ web-folder/wallet-web-app/src/pages/wallet/Send.tsx
@@ -1,6 +1,8 @@
 import { useForm, isNotEmpty, isEmail, isInRange, hasLength, matches } from '@mantine/form';
 import { Button, Group, TextInput, NumberInput, Box, Textarea } from '@mantine/core';
 import { useMantaStore } from '../_app';
+import { trpc } from '../../utils/trpc';
+import { useCallback, useEffect } from 'react';
 
 const Send = () => {
   const form = useForm({
@@ -20,14 +22,25 @@ const Send = () => {
   });
 
   const send = useMantaStore((state) => state.send)
-  const handleSend = () => {
-      try {
-        if (form.values.amount && form.values.sender && form.values.receiver && form.values.description)
-       send(form.values.amount) 
-      } catch (e) {
-       console.log(e) 
+
+  const username = useMantaStore((state) => state.username)
+  const send_money = trpc.payment.useMutation({ onSuccess: async () => { return console.log("success") } });
+
+  const handleSend = useCallback(() => {
+    try {
+      if (form.values.amount !== 0 && form.values.sender === username && form.values.receiver !== "" && form.values.description !== "") {
+        send_money.mutate({
+          amount: `${form.values.amount}`,
+          sender: form.values.sender,
+          receiver: form.values.receiver,
+          description: form.values.description
+        })
+        send(form.values.amount)
       }
+    } catch (e) {
+      console.log(e)
     }
+  }, [send_money, form.values.amount, form.values.sender, form.values.receiver, form.values.description]);
 
   return (
     <Box component="form" maw={400} mx="auto" onSubmit={form.onSubmit(() => { })}>
