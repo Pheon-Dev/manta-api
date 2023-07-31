719caadff1311391645144b586bdb3d8119f436d
Pheon-Dev <devpheon@gmail.com>
2023-07-31 22:00:45 +0300

    [-] Utilities

--- web-folder/wallet-web-app/src/components/index.ts
+++ web-folder/wallet-web-app/src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as NavBar } from './NavBar';
-export { default as Utilities } from './Utilities';
 export { default as Login } from './Login';
 export { default as ColorSchemeToggle } from './ColorSchemeToggle';
