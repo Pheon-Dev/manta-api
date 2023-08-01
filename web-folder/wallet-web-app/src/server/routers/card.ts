e92dd63c058b87e96432cdf6a9ada1a972de83b2
Pheon-Dev <devpheon@gmail.com>
2023-08-02 02:47:56 +0300

    [~] creating a card parameters

--- web-folder/wallet-web-app/src/server/routers/card.ts
+++ web-folder/wallet-web-app/src/server/routers/card.ts
@@ -3,16 +3,20 @@ import { z } from 'zod';
 import { procedure, router } from '../trpc';
 
 export const cardRouter = router({
-  cards: procedure
+  create: procedure
     .input(
       z.object({
         cookie: z.string(),
         method: z.string(),
         id: z.number(),
-        amount: z.string(),
-        sender: z.string(),
-        receiver: z.string(),
-        description: z.string(),
+        cname: z.string(),
+        cbalance: z.string(),
+        ctype: z.string(),
+        caccount: z.string(),
+        cnumber: z.string(),
+        cvv: z.string(),
+        cvalid: z.string(),
+        cdescription: z.string(),
       }),
     )
     .mutation(async (opts) => {
@@ -23,7 +27,7 @@ export const cardRouter = router({
       const headers = {
         Cookie: cookie
       }
-      let payments = await axios.request({
+      let response = await axios.request({
         method: "POST",
         url,
         headers,
@@ -32,17 +36,61 @@ export const cardRouter = router({
           method,
           params: {
             data: {
-              amount: `${opts.input.amount}`,
-              sender: `${opts.input.sender}`,
-              receiver: `${opts.input.receiver}`,
-              description: `${opts.input.description}`,
+              cname: `${opts.input.cname}`,
+              cbalance: `${opts.input.cbalance}`,
+              ctype: `${opts.input.ctype}`,
+              caccount: `${opts.input.caccount}`,
+              cnumber: `${opts.input.cnumber}`,
+              cvv: `${opts.input.cvv}`,
+              cvalid: `${opts.input.cvalid}`,
+              cdescription: `${opts.input.cdescription}`,
             }
           }
         }
       });
 
       return {
-        payments: payments.data,
+        response: response.data,
       };
+    }),
+  list: procedure
+    .input(
+      z.object({
+        id: z.number(),
+        method: z.string(),
+        cookie: z.string(),
+      }),
+    )
+    .query(async (opts) => {
+      const method = `${opts.input.method}`
+      const cookie = `${opts.input.cookie}`
+      const id = opts.input.id
+      const url = "http://localhost:8080/api/rpc"
+      const headers = {
+        Cookie: cookie
+      }
+      try {
+
+        let data = await axios.request({
+          method: "POST",
+          url,
+          headers,
+          data: {
+            id,
+            method,
+          }
+        });
+
+        return {
+          data: data.data,
+        };
+      } catch (error) {
+        return {
+          error: error,
+          message: "Internal Server Error",
+          solution: "Login to Server or Check your internet connection"
+        }
+
+      }
     }),
 });
