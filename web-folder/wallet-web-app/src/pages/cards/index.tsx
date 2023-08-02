2721bfa9e57a6b331a7a38b9d30e4aef374fcd96
Pheon-Dev <devpheon@gmail.com>
2023-08-02 19:17:42 +0300

    [~] card UI

--- web-folder/wallet-web-app/src/pages/cards/index.tsx
+++ web-folder/wallet-web-app/src/pages/cards/index.tsx
@@ -6,82 +6,38 @@ const Cards = () => {
   const { status, data } = useSession();
   const name = data?.user?.name;
   const account = trpc.card.list.useQuery({ method: "list_cards", id: 1, cookie: `${data?.user?.image}` });
-  const res = account?.data?.data?.result?.data[0]
+  const res = account?.data?.data?.result?.data
   return (
     <Grid>
+    {res && res.map((card) => (
+          <>
       <Grid.Col span={4}>
         <Card shadow="sm" padding="lg" radius="md" withBorder>
           <Group position="apart" mt="md" mb="xs">
-            <Text weight={500}>M-PESA</Text>
+            <Text weight={500}>{card.cname}</Text>
             <Badge color="violet" variant="light">
-              DEBIT
+              {`KES ${card.cbalance}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
             </Badge>
           </Group>
 
-          <Text size="sm" color="dimmed">
-            M-PESA Global Pay Card
-          </Text>
-
           <Button variant="light" color="blue" fullWidth mt="md" radius="md">
-            4597-XXXX-XXXX-XXXX-XXXX
+            {card.cnumber.slice(0, 4)} **** **** ****
           </Button>
+          <Text size="sm" color="dimmed">
+            {card.cdescription}
+          </Text>
+          <Group position="apart">
+            <Text size="sm" weight={100}>CVV: {card.cvv}</Text>
+            <Text size="sm" weight={100}>VALID: {card.cvalid}</Text>
+            </Group>
+          <Group position="right">
+            <Text size="sm" weight={300}>{card.caccount}</Text>
+            </Group>
+
         </Card>
       </Grid.Col>
-      <Grid.Col span={4}>
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Group position="apart" mt="md" mb="xs">
-            <Text weight={500}>Master Card</Text>
-            <Badge color="pink" variant="light">
-              CREDIT
-            </Badge>
-          </Group>
-
-          <Text size="sm" color="dimmed">
-            Master Card Credit Card
-          </Text>
-
-          <Button variant="light" color="blue" fullWidth mt="md" radius="md">
-            4499-XXXX-XXXX-XXXX-XXXX
-          </Button>
-        </Card>
-      </Grid.Col>
-      <Grid.Col span={4}>
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Group position="apart" mt="md" mb="xs">
-            <Text weight={500}>VISA Card</Text>
-            <Badge color="violet" variant="light">
-              DEBIT
-            </Badge>
-          </Group>
-
-          <Text size="sm" color="dimmed">
-            VISA Debit Card
-          </Text>
-
-          <Button variant="light" color="blue" fullWidth mt="md" radius="md">
-            4897-XXXX-XXXX-XXXX-XXXX
-          </Button>
-        </Card>
-      </Grid.Col>
-      <Grid.Col span={4}>
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Group position="apart" mt="md" mb="xs">
-            <Text weight={500}>KCB</Text>
-            <Badge color="violet" variant="light">
-              DEBIT
-            </Badge>
-          </Group>
-
-          <Text size="sm" color="dimmed">
-            Kenya Commercial Bank Debit Card
-          </Text>
-
-          <Button variant="light" color="blue" fullWidth mt="md" radius="md">
-            4327-XXXX-XXXX-XXXX-XXXX
-          </Button>
-        </Card>
-      </Grid.Col>
-        <pre>{JSON.stringify(account.data, undefined, 2)}</pre>
+          </>
+    )) || null}
     </Grid>
   )
 }
