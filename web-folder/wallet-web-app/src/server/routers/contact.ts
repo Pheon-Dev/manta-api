3b1ef119ca834e9757cb2375edd0b3ef5c24ad26
Pheon-Dev <devpheon@gmail.com>
2023-08-02 20:52:43 +0300

    [+] create and list contacts

--- web-folder/wallet-web-app/src/server/routers/contact.ts
+++ web-folder/wallet-web-app/src/server/routers/contact.ts
@@ -3,19 +3,16 @@ import { z } from 'zod';
 import { procedure, router } from '../trpc';
 
 export const contactRouter = router({
-  contacts: procedure
+  list: procedure
     .input(
       z.object({
         cookie: z.string(),
         method: z.string(),
         id: z.number(),
-        amount: z.string(),
-        sender: z.string(),
-        receiver: z.string(),
-        description: z.string(),
       }),
     )
-    .mutation(async (opts) => {
+    .query(async (opts) => {
+    try {
       const method = `${opts.input.method}`
       const cookie = `${opts.input.cookie}`
       const id = opts.input.id
@@ -30,12 +27,56 @@ export const contactRouter = router({
         data: {
           id,
           method,
+        }
+      });
+
+      return {
+        payments: payments.data,
+      };
+      } catch (error) {
+        return {
+          error: error,
+          message: "Internal Server Error check connection",
+        }
+      }
+    }),
+  create: procedure
+    .input(
+      z.object({
+        cookie: z.string(),
+        method: z.string(),
+        id: z.number(),
+        ref_id: z.string(),
+        association: z.string(),
+        username: z.string(),
+        name: z.string(),
+        email: z.string(),
+      }),
+    )
+    .mutation(async (opts) => {
+    try {
+      const method = `${opts.input.method}`
+      const cookie = `${opts.input.cookie}`
+      const id = opts.input.id
+      const url = "http://localhost:8080/api/rpc"
+      const headers = {
+        Cookie: cookie
+      }
+      
+      let payments = await axios.request({
+        method: "POST",
+        url,
+        headers,
+        data: {
+          id,
+          method,
           params: {
             data: {
-              amount: `${opts.input.amount}`,
-              sender: `${opts.input.sender}`,
-              receiver: `${opts.input.receiver}`,
-              description: `${opts.input.description}`,
+              ref_id: `${opts.input.ref_id}`,
+              association: `${opts.input.association}`,
+              username: `${opts.input.username}`,
+              name: `${opts.input.name}`,
+              email: `${opts.input.email}`,
             }
           }
         }
@@ -44,6 +85,12 @@ export const contactRouter = router({
       return {
         payments: payments.data,
       };
+      } catch (error) {
+        return {
+          error: error,
+          message: "Internal Server Error check connection",
+        }
+      }
     }),
 });
 
