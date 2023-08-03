cb64ed1d477b83effbb045f47ec4afc920cb7e8c
Pheon-Dev <devpheon@gmail.com>
2023-08-03 09:12:05 +0300

    [+] set cid from navbar

--- web-folder/wallet-web-app/src/components/NavBar.tsx
+++ web-folder/wallet-web-app/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect  } from 'react';
+import { useEffect } from 'react';
 import { trpc } from '../utils/trpc';
 import { useSession, signOut } from 'next-auth/react';
 import { IconGauge, IconFingerprint, IconActivity, IconChevronRight, IconHome, IconCreditCard, IconMessageChatbot, IconApps, IconUsers } from '@tabler/icons-react';
@@ -6,6 +6,7 @@ import { Box, NavLink } from '@mantine/core';
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { notifications } from '@mantine/notifications';
+import { useMantaStore } from '../pages/_app';
 
 const nav_data = [
   {
@@ -52,18 +53,29 @@ const nav_data = [
   },
 ];
 
+interface Account {
+  email: string,
+  balance: string,
+  id: number,
+  username: string,
+  aid: string,
+}
 const NavBar = () => {
   const router = useRouter();
   const { status, data } = useSession();
   const account = trpc.account.list.useQuery({ method: "list_accounts", id: 1, cookie: `${data?.user?.image}` });
 
+  const setID = useMantaStore((state) => state.setID)
+  const res = account?.data?.data?.result?.data.find((account: Account) => account.email === data?.user?.email);
+
   useEffect(() => {
     let sub = true
     if (sub) {
+      setID(res?.cid)
       if (account?.data?.error) signOut();
     }
-    return () => {sub = false}
-  }, [account?.data?.error])
+    return () => { sub = false }
+  }, [res?.cid, account?.data?.error])
 
   const items = nav_data.map((item, index) => (
     <Link key={index} href={`${item.view}`} style={{ textDecoration: 'none' }}>
